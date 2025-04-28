2b7536fbaafa77a4f4bae2fb6ab28c26c045dc4b
Connor Rhodes <connor@rhodes.contact>
2025-04-27 20:48:56 -0500

    update shell

--- default.nix
+++ default.nix
@@ -2,13 +2,13 @@
 let
   gems = bundlerEnv {
     name = "your-package";
-    inherit ruby;
+    inherit ruby_3_4;
     gemdir  = ./.;
   };
 in stdenv.mkDerivation {
   name = "your-package";
   src = ./.;
-  buildInputs = [gems ruby];
+  buildInputs = [gems ruby_3_4];
   installPhase = ''
     mkdir -p $out
     cp -r $src $out
--- shell.nix
+++ shell.nix
@@ -2,18 +2,17 @@
 with pkgs;
 with stdenv;
 let
-  app = bundlerEnv {
+  httparty = bundlerEnv {
     name = "httparty";
     ruby = ruby_3_4;
     gemdir = ./.;
   };
 in mkShell {
   name = "bundler-shell";
-  buildInputs = [app bundix ruby_3_4];
-  packages = [ 
-    pkgs.cowsay 
-    pkgs.rubyPackages_3_4.pry
-    app
+  buildInputs = [httparty bundix ruby_3_4];
+  packages = with pkgs; [ 
+    cowsay 
+    rubyPackages_3_4.pry
   ];
 }
 
