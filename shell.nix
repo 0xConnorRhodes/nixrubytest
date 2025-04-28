b6b8648d83e2268c00291f27ebad13301d444c06
Connor Rhodes <connor@rhodes.contact>
2025-04-27 19:47:02 -0500

    shell with working require

--- shell.nix
+++ shell.nix
@@ -1,8 +1,20 @@
-with import <nixpkgs> { };
-ruby.withPackages (
-  ps: with ps; [
-    pry
-    dotenv
-    (callPackage ./. { })
-  ]
-)
+# with import <nixpkgs> { };
+# ruby.withPackages (
+#   ps: with ps; [
+#     pry
+#     dotenv
+#     (callPackage ./. { })
+#   ]
+# )
+
+with (import <nixpkgs> {});
+let
+  gems = bundlerEnv {
+    name = "httparty";
+    inherit ruby;
+    gemdir = ./.;
+  };
+in stdenv.mkDerivation {
+  name = "your-package";
+  buildInputs = [gems ruby];
+}
